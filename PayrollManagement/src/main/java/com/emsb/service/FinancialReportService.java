2d14547647855703c10c2d6c03d410b7843d91e7
Coder_Dheeraj/ <113779503+Dheeraj475@users.noreply.github.com>
2024-08-23 11:54:07 +0530

    addd

--- PayrollManagement/src/main/java/com/emsb/service/FinancialReportService.java
+++ PayrollManagement/src/main/java/com/emsb/service/FinancialReportService.java
@@ -1,16 +1,13 @@
 package com.emsb.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
-import com.emsb.repository.PayrollRepository;
+
 
 @Service
 public class FinancialReportService {
-	
-	@Autowired
-	private PayrollRepository payrollRepo;
-	
-	
 
+   
 }
